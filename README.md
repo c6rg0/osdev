3f16cd69ca728b7304b57166d6f212d557b5847f
c6rg0 <gdrozbik08@gmail.com>
2026-06-30 20:52:24 +0100

    ISO build: bash -> flake, now using nix TMPDIR instead of target/, heavily improved README, added showcase image and osdev wiki license

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
-# Bare-metal learning repo
-- Use a UNIX(-like) OS to build (Linux, Macos, BSD, ...)
-- Package manager personally used: nix
-- Dependencies in flake.nix can be installed manually or with `$ nix develop`
+# 32-bit (i386) freestanding console
+- Kernel init and kernel (which is just a VGA driver).
+- Credits for tutorial and tasks: [wiki.osdev.org](https://wiki.osdev.org/Bare_Bones).
+- The MIT license for the tutorial is in the root of the directory.
 
-# Build instructions
-- Use build.sh to build (I'll set up "make" later),
-- To use iso: `$ qemu-system-i386 -cdrom target/myos.iso`.
+![showcase](showcase.png)
+
+## NOTES:
+- $ = shell command (assuming the use of bash && gnu coreutils).
+- For windows users: use WSL with nixos and follow the nix instructions.
+- You might have to bootstrap the cross-compiler (which can take some time).
+
+## How to build with nix pacakge manager
+1. Have nix(os) installed (supports Linux and Macos),
+2. Download the repo:
+  - `$ git clone https://github.com/c6rg0/osLearning.git`
+  - `$ cd osLearning`
+2. `$ nix build`
+
+### Manually
+1. Use the `flake.nix` to read the dependencies:
+  - `nativeBuildInputs`: compile-time dependencies. `buildInputs`: run-time dependencies.
+2. (still in the flake) Use the commands in all phases: 
+  - They won't work to the dot since some directories are specific to nix.
+
+## How to use the fresh iso
+- On Linux, macOS and BSD, you can use qemu: `$ qemu-system-i386 -cdrom result/bin/myos.iso`.
+  - (if you done it the nix way) Using `$ nix develop` installs qemu for you.
+- Or flash the iso on a USB and use it on hardware.
 
--- flake.nix
+++ flake.nix
@@ -1,15 +1,71 @@
+# Credits:
+# https://parkerjones.dev/posts/mastering-c-nix-flake/
+
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-  outputs = { self, nixpkgs }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      nativeBuildInputs = [
-        pkgs.pkgsCross.i686-embedded.buildPackages.gcc
-        pkgs.grub2
-        pkgs.libisoburn
-        pkgs.qemu
-      ];
+  description = "OS dev";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages = nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    rec {
+      myos = pkgs.stdenv.mkDerivation {
+        pname = "myos";
+        version = "0.0.1";
+
+        src = ./src;
+
+        nativeBuildInputs = with pkgs; [ 
+          pkgsCross.i686-embedded.buildPackages.gcc
+          grub2
+          libisoburn
+        ];
+
+        buildInputs = with pkgs; [ 
+          qemu
+        ];
+
+        phases = [ "preBuildPhase" "buildPhase" "preInstallPhase" "installPhase" ];
+
+        preBuildPhase = ''
+          cd $src
+          pwd
+        '';
+
+        buildPhase = '' 
+          i686-elf-as boot.s -o $TMPDIR/boot.o
+          i686-elf-gcc -c kernel.c -o $TMPDIR/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+          i686-elf-gcc -T linker.ld -o $TMPDIR/myos -ffreestanding -O2 -nostdlib $TMPDIR/boot.o $TMPDIR/kernel.o -lgcc
+        '';
+
+        preInstallPhase = ''
+          mkdir -p $out/bin
+          mkdir -p $TMPDIR/isodir/boot/grub/
+          cp grub.cfg $TMPDIR/isodir/boot/grub/grub.cfg
+          cp $TMPDIR/myos $TMPDIR/isodir/boot/myos
+        '';
+
+        installPhase = ''
+          grub-mkrescue -o $TMPDIR/myos.iso $TMPDIR/isodir
+          mv $TMPDIR/myos.iso $TMPDIR/myos $out/bin/
+        '';
+
+        meta = with pkgs.lib; {
+          description = "An OS dev test";
+          license = licenses.mit;
+          maintainers = [ maintainers.c6rg0 ];
+          platforms = platforms.unix;
+        };
+      };
+    });
+
+    defaultPackage = {
+      x86_64-linux = self.packages.x86_64-linux.myos;
     };
   };
 }
