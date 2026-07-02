27fa7bdad7333a1dd25eb345a6f590a9a684e2a0
c6rg0 <gdrozbik08@gmail.com>
2026-07-02 17:13:43 +0100

    Added io: outb and inb

---
+++ src/io.c
@@ -0,0 +1,19 @@
+#include "io.h"
+
+// https://en.wikipedia.org/wiki/Memory-mapped_I/O_and_port-mapped_I/O
+// https://wiki.osdev.org/Inline_Assembly/Examples#I.2FO_access
+
+extern inline void outb(uint16_t port, uint8_t val)
+{
+    __asm__ volatile ( "outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
+}
+
+extern inline uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    __asm__ volatile ( "inb %w1, %b0"
+                   : "=a"(ret)
+                   : "Nd"(port)
+                   : "memory");
+    return ret;
+}
