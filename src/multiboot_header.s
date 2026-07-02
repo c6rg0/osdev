53ceac2575a2d8acf16a5e098c38770a3cd8fbd7
c6rg0 <gdrozbik08@gmail.com>
2026-07-02 16:59:46 +0100

    Split up boot.s -> multiboot_header.s and prekernel.s

--- src/multiboot_header.s
+++ src/multiboot_header.s
@@ -11,28 +11,3 @@
 .long MAGIC
 .long FLAGS
 .long CHECKSUM
-
-// Defining the value of the stack pointer
-.section .bss
-.align 16
-stack_bottom:
-.skip 16384 # 16 KiB
-stack_top:
-
-.section .text
-.global _start
-.type _start, @function
-_start:
-	mov $stack_top, %esp
-
-	call kernel_main
-
-	cli
-1:	hlt
-	jmp 1b
-
-/*
-Set the size of the _start symbol to the current location '.' minus its start.
-This is useful when debugging or when you implement call tracing.
-*/
-.size _start, . - _start
---
+++ src/prekernel.s
@@ -0,0 +1,27 @@
+// Defining the value of the stack pointer
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384 # 16 KiB
+stack_top:
+
+.section .text
+.global _start
+.type _start, @function
+_start:
+	mov $stack_top, %esp
+
+    // Hardware/arch init before calling kernel:
+
+    // Then kernel can use the hardware init
+	call kernel_main
+
+	cli
+1:	hlt
+	jmp 1b
+
+/*
+Set the size of the _start symbol to the current location '.' minus its start.
+This is useful when debugging or when you implement call tracing.
+*/
+.size _start, . - _start
